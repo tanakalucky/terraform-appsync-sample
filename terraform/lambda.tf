c74de3cf155a2dd3087f7ac2c338c5126429e69f
tanakalucky <39301079+tanakalucky@users.noreply.github.com>
2024-08-25 03:02:06 +0000

    fix: Add dynamodb access policy to lambda

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -9,9 +9,10 @@ module "get_data" {
   attach_policies = true
   policies = [
     "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
-    "arn:aws:iam::aws:policy/AWSAppSyncInvokeFullAccess"
+    "arn:aws:iam::aws:policy/AWSAppSyncInvokeFullAccess",
+    "arn:aws:iam::aws:policy/AmazonDynamoDBReadOnlyAccess"
   ]
-  number_of_policies = 2
+  number_of_policies = 3
 }
 
 module "put_data" {
@@ -25,7 +26,8 @@ module "put_data" {
   attach_policies = true
   policies = [
     "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
-    "arn:aws:iam::aws:policy/AWSAppSyncInvokeFullAccess"
+    "arn:aws:iam::aws:policy/AWSAppSyncInvokeFullAccess",
+    "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
   ]
-  number_of_policies = 2
+  number_of_policies = 3
 }
